599ffed53fc2cab3ed1bcde218614eee2ccde244
Nubal Manhas <nubalmanhas@node-1w7jra2a12h9rrhsg8zl8afa6.ipv6.telus.net>
2025-10-13 15:01:03 -0600

    Added contact form with formspree

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,43 @@
 export default function Contact() {
+  const formEndPoint = import.meta.env.VITE_FORMSPREE_ENDPOINT;
+
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className="flex flex-col items-center justify-center p-10">
+      <form
+        action={formEndPoint}  // Replace with your actual Formspree endpoint
+        method="POST"
+        className="bg-[#FDF6FF] text-[#2D1E2F] p-6 rounded-xl shadow-md w-full max-w-xl space-y-4 ">
+        <h2 className="text-2xl font-bold mb-2">Contact Me</h2>
+
+        <input
+          type="text"
+          name="name"
+          placeholder="Your Name"
+          required
+          className="w-full p-2 border border-gray-300 rounded-md"
+        />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Your Email"
+          required
+          className="w-full p-2 border border-gray-300 rounded-md"
+        />
+
+        <textarea
+          name="message"
+          placeholder="Your Message"
+          required
+          className="w-full p-2 border border-gray-300 rounded-md h-32"
+        />
+
+        <button
+          type="submit"
+          className="bg-[#2D1E2F] text-[#FDF6FF] px-4 py-2 rounded-md hover:bg-[#C08497] transition">
+          Send Inquiry
+        </button>
+      </form>
     </div>
   )
 }
