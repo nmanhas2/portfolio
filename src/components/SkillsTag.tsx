20e327cf491bd587c3ad65af2a401ebaedcda62e
Nubal Manhas <nubalmanhas@node-1w7jra2a12h9rrhsg8zl8afa6.ipv6.telus.net>
2025-10-13 14:32:24 -0600

    Added a pointer effect on hover for skillstags, changed html title

--- src/components/SkillsTag.tsx
+++ src/components/SkillsTag.tsx
@@ -4,7 +4,7 @@ interface SkillsTagProps {
 
 export default function SkillsTag({skillName}: SkillsTagProps) {
   return (
-        <div className="inline-block border-4 bg-[#2D1E2F] text-[#FDF6FF] px-3 py-1 transition-all duration-300 hover:scale-105 hover:border-[#D8B4E2] hover:shadow-[0_0_10px_#FDF6FF]">
+        <div className="inline-block border-4 bg-[#2D1E2F] text-[#FDF6FF] px-3 py-1 transition-all duration-300 hover:scale-105 hover:border-[#D8B4E2] hover:cursor-pointer hover:shadow-[0_0_10px_#FDF6FF]">
         {skillName}
         </div>
   );
